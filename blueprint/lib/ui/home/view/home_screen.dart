a58c649f888a03b5de2657420ec31c5b74a7ab61
Jean Vallon <jean@hush-app.com>
2026-07-09 15:58:39 +0200

    test(home): implement fake repository methods in welcome dialog test

--- blueprint/lib/ui/home/view/home_screen.dart
+++ blueprint/lib/ui/home/view/home_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:snowflake_flutter_theme/snowflake_flutter_theme.dart';
 import 'package:touchstone/core/app/i18n/translations.g.dart';
-import 'package:touchstone/data/local/app_preferences.dart';
 import 'package:touchstone/core/theme/app_colors.dart';
+import 'package:touchstone/data/local/app_preferences.dart';
 import 'package:touchstone/domain/model/todo.dart';
 import 'package:touchstone/ui/home/controller/home_controller.dart';
 
@@ -36,9 +36,7 @@ class HomeScreen extends ConsumerWidget {
     return Init(
       onInitPostFrame: () => _showWelcomeDialog(context, ref),
       child: Scaffold(
-        appBar: AppBar(
-          title: AppText.l(t.homeScreen.todosTitle, bold: true),
-        ),
+        appBar: AppBar(title: AppText.l(t.homeScreen.todosTitle, bold: true)),
         body: Padding(
           padding: ThemeSizes.m.asInsets,
           child: Column(
