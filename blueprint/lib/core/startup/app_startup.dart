a168a07dd9db64e5f55587dde19e7378ae1a6faa
Jean Vallon <jean@hush-app.com>
2026-07-15 08:00:43 +0200

    clean: remove sentry failure tests

--- blueprint/lib/core/startup/app_startup.dart
+++ blueprint/lib/core/startup/app_startup.dart
@@ -6,8 +6,6 @@ import 'package:touchstone/core/log/log.dart';
 class AppStartup {
   static final futureProvider = FutureProvider<void>((ref) async {
     await ref.watch(AppPreferences.futureProvider.future);
-    throw StateError('This is a test for Sentry');
-    throw StateError('Test startup failure');
     'App startup completed'.logInfo;
     ref.onDispose(() {
       ref.invalidate(AppPreferences.futureProvider);
