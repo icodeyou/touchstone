3eac3b019ddce344f5279fc2b97c4f94895985cf
Jean Vallon <jean@hush-app.com>
2026-07-20 13:01:28 +0200

    fix: use random UUID for web version

--- blueprint/lib/core/startup/startup_future_providers.dart
+++ blueprint/lib/core/startup/startup_future_providers.dart
@@ -3,14 +3,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_udid/flutter_udid.dart';
 import 'package:touchstone/core/log/log.dart';
 import 'package:touchstone/core/startup/app_startup.dart';
-import 'package:touchstone/shared/constants/app_constants.dart';
+import 'package:uuid/uuid.dart';
 
 /// Async dependencies eagerly initialized by [AppStartup].
 class StartupFutureProviders {
   /// Stable identifier of the current install
   static final deviceId = FutureProvider<String>((ref) async {
-    const prefix = kDebugMode ? AppConstants.debugPrefix : '';
-    final deviceId = '$prefix${await FlutterUdid.udid}';
+    const prefix = kDebugMode ? 'debug_' : '';
+
+    final String deviceId;
+    if (kIsWeb) {
+      deviceId = '${prefix}web_${const Uuid().v4()}';
+    } else {
+      deviceId = '$prefix${await FlutterUdid.udid}';
+    }
     'Device ID: $deviceId'.logInfo;
     return deviceId;
   }, retry: (retryCount, error) => null);
--- blueprint/lib/shared/constants/app_constants.dart
+++ blueprint/lib/shared/constants/app_constants.dart
@@ -1,12 +1,11 @@
 class AppConstants {
-  // Debug
-  static const String debugPrefix = 'debug_';
   // ---------------------------------------------------------------------------
   // Logging
   static const int logStacktraceNumber = 5;
 
   /// Sentry DSN - By default, it is project "Pixelita"
-  static const String sentryDsn = 'https://3a136dc602b6f06602e1f9f87119d40c@o4511727383871488.ingest.de.sentry.io/4511727449669712';
+  static const String sentryDsn =
+      'https://3a136dc602b6f06602e1f9f87119d40c@o4511727383871488.ingest.de.sentry.io/4511727449669712';
   // ---------------------------------------------------------------------------
   // Network
   static const String goRestBaseUrl = 'https://gorest.co.in/public/v2';
